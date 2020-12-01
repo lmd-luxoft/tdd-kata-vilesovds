44529ceecbdf42b865271d23b7fdf785362224d7
Вилесов Дмитрий Сергеевич <d.vilesov@owen.ru>
2020-12-01 10:53:25 +0300

    added few tests fixed public method Add

--- TDDKata/test.cpp
+++ TDDKata/test.cpp
@@ -1,5 +1,113 @@
 #include "pch.h"
-	TEST(TDDKata, TestName) {
-		ASSERT_EQ(1, 1);
-		ASSERT_TRUE(true);
-	}
+#include "Calculator.h"
+TEST(TDDKata, PositiveUsual) {
+	// arrange
+	Calculator calc;
+	char* nums = "0,1,2";
+	// act
+	int res = calc.Add(nums);
+	//assert
+
+	ASSERT_EQ(res, 3);
+}
+
+TEST(TDDKata, PositiveZero) {
+	// arrange
+	Calculator calc;
+	char* nums = "0,0,0";
+	// act
+	int res = calc.Add(nums);
+	//assert
+
+	ASSERT_EQ(res, 0);
+}
+
+
+TEST(TDDKata, PositiveOne) {
+	// arrange
+	Calculator calc;
+	char* nums = "0";
+	// act
+	int res = calc.Add(nums);
+	//assert
+
+	ASSERT_EQ(res, 0);
+}
+
+TEST(TDDKata, PositiveTwo) {
+	// arrange
+	Calculator calc;
+	char* nums = "1,2";
+	// act
+	int res = calc.Add(nums);
+	//assert
+
+	ASSERT_EQ(res, 3);
+}
+
+TEST(TDDKata, PositiveEmpty) {
+	// arrange
+	Calculator calc;
+	char* nums = "";
+	// act
+	int res = calc.Add(nums);
+	//assert
+
+	ASSERT_EQ(res, 0);
+}
+
+TEST(TDDKata, NegativeNotNumAll) {
+	// arrange
+	Calculator calc;
+	char* nums = "abc";
+	// act
+	int res = calc.Add(nums);
+	//assert
+
+	ASSERT_EQ(res, -1);
+}
+
+TEST(TDDKata, NegativeNotNumOne) {
+	// arrange
+	Calculator calc;
+	char* nums = "a,1,2";
+	// act
+	int res = calc.Add(nums);
+	//assert
+
+	ASSERT_EQ(res, -1);
+}
+
+TEST(TDDKata, NegativeNotNumOneAtEnd) {
+	// arrange
+	Calculator calc;
+	char* nums = "1,2,a";
+	// act
+	int res = calc.Add(nums);
+	//assert
+
+	ASSERT_EQ(res, -1);
+}
+
+TEST(TDDKata, NegativeWrongSeparator) {
+	// arrange
+	Calculator calc;
+	char* nums = "1 2";
+	// act
+	int res = calc.Add(nums);
+	//assert
+
+	ASSERT_EQ(res, -2);
+}
+
+
+TEST(TDDKata, NegativeWrongCountOfNumbers) {
+	// arrange
+	Calculator calc;
+	char* nums = "1,2,3,4";
+	// act
+	int res = calc.Add(nums);
+	//assert
+
+	ASSERT_EQ(res, -3);
+}
